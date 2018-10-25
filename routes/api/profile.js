9a0ab7f65de4b57a8bb9cdfef2d82a3a874193dc
Mario Juretić <mario.juretic@mariojuretic.com>
2018-10-25 19:14:23 +0200

    GET /api/profile/all route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -35,6 +35,25 @@ router.get(
   }
 );
 
+// @route   GET /api/profile/all
+// @desc    Get all profiles
+// @access  Public
+router.get("/all", (req, res) => {
+  const errors = {};
+
+  Profile.find()
+    .populate("user", ["name", "avatar"])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofile = "There are no profiles";
+        return res.status(404).json(errors);
+      }
+
+      res.json(profiles);
+    })
+    .catch(err => res.status(404).json({ noprofile: "There are no profiles" }));
+});
+
 // @route   GET /api/profile/handle/:handle
 // @desc    Get profile by handle
 // @access  Public
@@ -70,7 +89,9 @@ router.get("/user/:user_id", (req, res) => {
 
       res.json(profile);
     })
-    .catch(err => res.status(404).json(err));
+    .catch(err =>
+      res.status(404).json({ noprofile: "There is no profile for this user" })
+    );
 });
 
 // @route   POST /api/profile
