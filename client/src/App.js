cf37cbf26641e4379b835bc5d34db0323da9c77c
Mario Juretić <mario.juretic@mariojuretic.com>
2018-10-29 20:21:26 +0100

    Finish login form functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -10,6 +13,16 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
 import "./App.css";
+
+// Check for Token
+if (localStorage.jwtToken) {
+  // Set Auth Header
+  setAuthToken(localStorage.jwtToken);
+  // Decode Token
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set User
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
